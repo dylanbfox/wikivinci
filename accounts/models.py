67efa7d858ee815ba7be37bb8a8be4445540162c
Dylan Fox <dylan@crowdvance.com>
2014-12-29 17:45:36 -0500

    simplifying things

--- accounts/models.py
+++ accounts/models.py
@@ -8,10 +8,10 @@ class Account(models.Model):
 	def __unicode__(self):
 		return self.owner.username
 
-	def save(self, *args, **kwargs):
-		if self.points == 0:
-			self.award_points(50)
-		super(Account, self).save(*args, **kwargs)		
+	# def save(self, *args, **kwargs):
+	# 	if self.points == 0:
+	# 		self.award_points(50)
+	# 	super(Account, self).save(*args, **kwargs)		
 
 	def award_points(self, points=0):
 		self.points += points
@@ -20,6 +20,6 @@ class Account(models.Model):
 	owner = models.OneToOneField(settings.AUTH_USER_MODEL)
 	profile_pic = models.ImageField(upload_to='profile_pics', default="static/core/images/default_profile_pic.jpg")
 	cropping = ImageRatioField('profile_pic', '250x250')
-	points = models.IntegerField(default=0)
+	points = models.IntegerField(default=50)
 
 
