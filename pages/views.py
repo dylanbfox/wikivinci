d74b0c66018d7af8e29a104c8a34711293bb5171
Dylan Fox <dylan@crowdvance.com>
2014-12-30 19:24:23 -0500

    contributors shell

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from accounts.models import Account
 from comments.models import Comment
 from posts.models import Post
 
@@ -8,3 +9,8 @@ def home(request):
 	context_dict['posts'] = Post.objects.select_related().all()[:5]
 	context_dict['comments'] = Comment.objects.select_related().order_by('-created')[:3]
 	return render(request, 'core/home.html', context_dict)
+
+def contributors(request):
+	context_dict = {}
+	context_dict['accounts'] = Account.objects.order_by('-points').select_related()
+	return render(request, 'core/contributors.html', context_dict)
