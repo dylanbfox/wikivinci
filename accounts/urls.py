8754e6741c7e65aacb652fba16d5077d5863ce8c
Dylan Fox <dylan@crowdvance.com>
2015-01-01 19:41:47 -0500

    logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ from accounts import views
 urlpatterns = patterns('',
 	url(r'^login/$', views.account_login, name='login'),
 	url(r'^register/$', views.account_register, name='register'),
+	url(r'^logout/$', views.account_logout, name='logout'),
 	url(r'^(?P<username>.+)/settings/$', views.settings, name='settings'),	
 	url(r'^(?P<username>.+)/$', views.profile, name='profile'),
 )
