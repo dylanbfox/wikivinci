4e566763cd25e9f5106d47f5b56106144d210995
Dylan Fox <dylan@crowdvance.com>
2015-01-03 18:10:20 -0500

    weaks

--- pages/views.py
+++ pages/views.py
@@ -3,10 +3,14 @@ from django.shortcuts import render
 from accounts.models import Account
 from comments.models import Comment
 from posts.models import Post
+from posts.utils import unique_topic_counts
 
 def home(request):
 	context_dict = {}
-	context_dict['posts'] = Post.objects.select_related().all()[:5]
+	posts = Post.objects.select_related().all().prefetch_related('upvoters', 'downvoters')
+	# topics = unique_topic_counts(posts)
+	# context_dict['topics'] = topics
+	context_dict['posts'] = posts[:5]
 	context_dict['comments'] = Comment.objects.select_related().order_by('-created')[:3]
 	return render(request, 'core/home.html', context_dict)
 
