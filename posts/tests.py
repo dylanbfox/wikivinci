699ace500a7e4bd589c7b87e6de5d1b63b8159c2
Dylan Fox <dylan@crowdvance.com>
2015-02-12 22:26:56 -0500

    placeholder tests for post videos

--- posts/tests.py
+++ posts/tests.py
@@ -1,5 +1,16 @@
 from django.test import TestCase
+from model_mommy import mommy
 
+class PostModelTest(TestCase):
 
+	def test_posts_can_be_saved_as_video(self):
+		pass
 
+class PostViewTest(TestCase):
+
+	def test_posts_add_view_saves_posts_as_link(self):
+		pass
+
+	def tests_posts_add_view_saves_posts_as_video(self):
+		pass
 
