bcfb9d0e9578a5a4580b9665720b96c405f93a94
Dylan Fox <dylan@crowdvance.com>
2015-02-18 15:10:44 -0500

    placeholder tests for posts views, hide coverage report from repo

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -1,11 +1,19 @@
 from django.test import TestCase
 from model_mommy import mommy
 
-class PostViewTest(TestCase):
+class ViewAllPostsView(TestCase):
 
-	def test_posts_add_view_saves_posts_as_link(self):
-		pass
+	def test_posts_filtered_by_GET_params(self):
+		self.fail()
 
-	def tests_posts_add_view_saves_posts_as_video(self):
-		pass
+	def test_GET_params_passed_to_template(self):
+		self.fail()
 
+	def test_top_GET_param_popped(self):
+		self.fail()
+
+	def test_posts_grouped_by_date(self):
+		self.fail()
+
+	def test_posts_sorted_by_vote_count(self):
+		self.fail()
