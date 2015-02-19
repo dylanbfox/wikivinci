3c0511a05f21509fc9014e1702e57088b6f38a74
Dylan Fox <dylan@crowdvance.com>
2015-02-19 14:57:42 -0500

    remove old Comment unit test

--- comments/tests.py
+++ comments/tests.py
@@ -26,17 +26,3 @@ class CommentReplyTest(TestCase):
 		replies_count = Comment.objects.filter(type='2').count()
 		self.assertEqual(0, replies_count)
 		self.assertEqual(1, comments_count)
-
-	def test_POST_to_add_view_saves_reply(self):
-		# call view w/ POST data
-		response = self.client.post(
-			reverse('comments:add'),
-			data={},
-		)
-		# check view responds with OK
-		self.assertEqual(response.status_code, 200)
-		# check DB has 1 type 2, 0 type 1
-		comments_count = Comment.objects.filter(type='1').count()
-		replies_count = Comment.objects.filter(type='2').count()
-		self.assertEqual(1, replies_count)
-		self.assertEqual(0, comments_count)
