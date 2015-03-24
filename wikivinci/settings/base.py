b9f28d236f677870d51bce3b0614498e7c346856
Dylan Fox <dylan@crowdvance.com>
2015-03-24 17:44:35 -0400

    public settings

---
+++ wikivinci/settings/base.py
@@ -0,0 +1,93 @@
+"""
+Django settings for wikivinci project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+
+ADMINS = (('Dylan', 'dylanbfox@gmail.com'),)
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'boto',
+    'storages',
+    'image_cropping',
+    'easy_thumbnails',
+    'posts',
+    'accounts',
+    'comments',
+    'pages',
+    'topics',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'wikivinci.urls'
+
+WSGI_APPLICATION = 'wikivinci.wsgi.application'
+
+# establish a project level template directory
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
+
+# Static files (CSS, JavaScript, Images)
+# establish a project level static directory
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# needed for whitenoise
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/New_York'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+from easy_thumbnails.conf import Settings as thumbnail_settings
+THUMBNAIL_PROCESSORS = (
+    'image_cropping.thumbnail_processors.crop_corners',
+) + thumbnail_settings.THUMBNAIL_PROCESSORS
+
+# Twitter
+TWITTER_APP_KEY = os.environ['TWITTER_APP_KEY']
+TWITTER_APP_SECRET = os.environ['TWITTER_APP_KEY']
---
+++ wikivinci/settings/local.py
@@ -0,0 +1,51 @@
+from base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS += (
+	'debug_toolbar',
+)
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "wikivinci",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
+# Media settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Celery
+BROKER_URL = os.environ['BROKER_URL']
+CELERY_ALWAYS_EAGER = False
+
+if CELERY_ALWAYS_EAGER:
+	print "*************************"
+	print "[WARNING] CELERY_ALWAYS_EAGER is set to True!"
+	print "*************************"	
+
+# Email settings
+EMAIL_HOST = "smtp.gmail.com" # gmail on local
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME'] # gmail on local
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD'] # gmail on local
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "Leonardo da Wikivinci <leo@wikivinci.com>"
+SERVER_EMAIL = '[PROD] error@wikivinci.com <error@wikivinci.com>'
+
+# Misc. settings
+HOST_NAME = 'http://4cb45e0f.ngrok.com'
